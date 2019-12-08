fd7a7db5fce185d3b6f7e9d790789d093bddb9ba
Ting-APG <57277390+Ting-APG@users.noreply.github.com>
2019-12-08 20:02:10 +0800

    Add files via upload

---
+++ Problem 1.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+def f(n):
+    if n<= 9:
+         return (n**2)-7
+    else:
+         return f(n-10)
+x = []
+y = []
+for n in range(1,100):
+   x.append(n)
+   y.append(f(n))
+   print(x)
+   print(y)
+   plt.stem(x,y)
+   plt.xlabel('n')
+   plt.ylabel('f(n)')
+   plt.title( 'The graph for f(n)' )
+   plt.show()
